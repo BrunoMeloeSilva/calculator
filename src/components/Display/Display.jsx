b44d5cad422fdc59e66fdbca6f8aa391e9e1a9bd
brunomeloesilva <brunomeloesilva@gmail.com>
2020-10-12 05:49:37 -0400

    organizacao de codigo

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -1,8 +1,14 @@
 import React from "react"
 import "./Display.css"
 
+
 function Display(params) {
-    return <input className="painel__display" type="number" readOnly {...params}/>
+
+    return <input 
+    className="painel__display" 
+    type="number" 
+    readOnly 
+    {...params}/>
 }
 
 export default Display
